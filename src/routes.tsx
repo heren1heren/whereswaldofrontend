12e6aed0ec9b216abaf4f6d249af04aceebd8017
heren <heren1heren@gmail.com>
2024-06-25 11:48:53 -0600

    change fetch links

--- src/routes.tsx
+++ src/routes.tsx
@@ -17,7 +17,7 @@ const routes = [
     element: (
       <GamePage
         title="The gold"
-        imageUrl="http://localhost:3000/images/gold.jpg"
+        imageUrl="https://veil-selective-witch.glitch.me/images/gold.jpg"
       />
     ),
   },
@@ -26,7 +26,7 @@ const routes = [
     element: (
       <GamePage
         title="The beach"
-        imageUrl="http://localhost:3000/images/beach.jpg"
+        imageUrl="https://veil-selective-witch.glitch.me/images/beach.jpg"
       />
     ),
   },
@@ -35,7 +35,7 @@ const routes = [
     element: (
       <GamePage
         title="The blue"
-        imageUrl="http://localhost:3000/images/blue.jpg"
+        imageUrl="https://veil-selective-witch.glitch.me/images/blue.jpg"
       />
     ),
   },
@@ -44,7 +44,7 @@ const routes = [
     element: (
       <GamePage
         title="The maze"
-        imageUrl="http://localhost:3000/images/maze.jpg"
+        imageUrl="https://veil-selective-witch.glitch.me/images/maze.jpg"
       />
     ),
   },
@@ -53,7 +53,7 @@ const routes = [
     element: (
       <GamePage
         title="The White"
-        imageUrl="http://localhost:3000/images/white.jpg"
+        imageUrl="https://veil-selective-witch.glitch.me/images/white.jpg"
       />
     ),
   },
@@ -62,7 +62,7 @@ const routes = [
     element: (
       <GamePage
         title="The Space"
-        imageUrl="http://localhost:3000/images/space.jpg"
+        imageUrl="https://veil-selective-witch.glitch.me/images/space.jpg"
       />
     ),
   },
